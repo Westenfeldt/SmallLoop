053c21bbfd1afee440d4c7b6c6d8e48f371f17af
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-22 11:59:34 +0200

    Add project files.

---
+++ SmallLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmallLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =1; i <=20; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int whileI = 20;
+
+            do
+            {
+                whileI++;
+                Console.WriteLine(whileI);
+            } while (whileI <= 39);
+            int whileINT = 41;
+            while( whileINT <=60)
+            {
+                Console.WriteLine(whileINT);
+                whileINT++;
+            }
+
+        }
+    }
+}
